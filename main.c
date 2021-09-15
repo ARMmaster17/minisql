3fa2873f6fa1f5b3bb5e020291019c0259f94484
Joshua Zenn <wordman05@gmail.com>
2021-09-15 08:17:26 -0400

    Part 5 and resolved segmentation fault error

--- main.c
+++ main.c
@@ -166,6 +166,45 @@ void pager_flush(Pager *pager, uint32_t page_num, uint32_t size) {
     }
 }
 
+void db_close(Table *table) {
+    Pager *pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++) {
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != NULL) {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1) {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        void *page = pager->pages[i];
+        if (page) {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+    free(pager);
+    free(table);
+}
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     if(strcmp(input_buffer->buffer, ".exit") == 0) {
         db_close(table);
@@ -209,52 +248,14 @@ void* get_page(Pager* pager, uint32_t page_num) {
                 exit(EXIT_FAILURE);
             }
         }
+        pager->pages[page_num] = page;
     }
     return pager->pages[page_num];
 }
 
-void db_close(Table *table) {
-    Pager *pager = table->pager;
-    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
-
-    for (uint32_t i = 0; i < num_full_pages; i++) {
-        if (pager->pages[i] == NULL) {
-            continue;
-        }
-        pager_flush(pager, i, PAGE_SIZE);
-        free(pager->pages[i]);
-        pager->pages[i] = NULL;
-    }
-
-    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
-    if (num_additional_rows > 0) {
-        uint32_t page_num = num_full_pages;
-        if (pager->pages[page_num] != NULL) {
-            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
-            free(pager->pages[page_num]);
-            pager->pages[page_num] = NULL;
-        }
-    }
-
-    int result = close(pager->file_descriptor);
-    if (result == -1) {
-        printf("Error closing db file.\n");
-        exit(EXIT_FAILURE);
-    }
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-        void *page = pager->pages[i];
-        if (page) {
-            free(page);
-            pager->pages[i] = NULL;
-        }
-    }
-    free(pager);
-    free(table);
-}
-
 void* row_slot(Table* table, uint32_t row_num) {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
+    void *page = get_page(table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
@@ -303,7 +304,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 }
 
 Pager* pager_open(const char* filename) {
-    int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | I_IRUSR);
+    int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
     if(fd == -1) {
         printf("Unable to open file\n");
         exit(EXIT_FAILURE);
@@ -325,13 +326,6 @@ Table* db_open(const char* filename) {
     table->pager = pager;
     table->num_rows = num_rows;
     return table;
-}
-
-void free_table(Table* table) {
-    for (int i = 0; table->pages[i]; i++) {
-        free(table->pages[i]);
-    }
-    free(table);
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
